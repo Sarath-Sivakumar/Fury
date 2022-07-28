1c6cdf8a5de79042a7bfaa1d473c9a5895854649
Sarath-Sivakumar <sarathputhenvila@gmail.com>
2022-07-28 18:02:54 +0530

    First commit

---
+++ app/src/main/java/app/personal/fury3/MainActivity.java
@@ -0,0 +1,17 @@
+package app.personal.fury3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //this is a comment
+    //this is also a comment
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
