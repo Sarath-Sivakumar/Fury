c16609f9044304f6d4c91c6197fcd5d7268b575d
Rohith Roy <rohith.roy393@gmail.com>
2023-02-22 14:11:54 +0530

    commit 133

--- app/src/main/java/app/personal/fury/ViewPagerAdapter/infoGraphicsAdapter.java
+++ app/src/main/java/app/personal/fury/ViewPagerAdapter/infoGraphicsAdapter.java
@@ -5,7 +5,9 @@ import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,34 +16,44 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 import app.personal.fury.R;
 
-public class infoGraphicsAdapter extends FragmentPagerAdapter {
-    private final ArrayList<Fragment> fragments;
+public class infoGraphicsAdapter extends PagerAdapter {
 
-    public infoGraphicsAdapter(@NonNull FragmentManager fm) {
-        super(fm);
-        this.fragments = new ArrayList<>();
-    }
+    private final Context context;
+    private final int[] images;
+    private final LayoutInflater inflater;
 
-    public void setInfoGraphics(ArrayList<Fragment> fragments){
-        this.fragments.addAll(fragments);
-    }
-
-    @Override
-    public void setPrimaryItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        super.setPrimaryItem(container, position, object);
-    }
-
-    @NonNull
-    @Override
-    public Fragment getItem(int position) {
-        return fragments.get(position);
+    public infoGraphicsAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-        return fragments.size();
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == ((FrameLayout) object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+        View itemView = inflater.inflate(R.layout.fragment_info_graphics, container, false);
+        ImageView imageView = itemView.findViewById(R.id.infoGraphics);
+        imageView.setImageResource(images[position]);
+        Objects.requireNonNull(container).addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((FrameLayout) object);
     }
 }
