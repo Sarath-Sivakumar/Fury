0cfbef816d7cf79aa3a510ff9e6cfc373116ac6b
Sarath Sivakumar <sarathputhenvila@gmail.com>
2023-03-01 16:57:29 +0530

    theme 2.1

--- app/src/main/java/app/personal/fury/UI/Drawer/Settings_Activity.java
+++ app/src/main/java/app/personal/fury/UI/Drawer/Settings_Activity.java
@@ -3,15 +3,21 @@ package app.personal.fury.UI.Drawer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -29,6 +35,8 @@ public class Settings_Activity extends AppCompatActivity {
     private ImageView profilePic;
     private TextView profileName;
     private EditText profileNameEdit;
+
+    private Button save,discard;
 
     // Uri indicates, where the image will be picked from
     private Uri filePath;
@@ -70,16 +78,53 @@ public class Settings_Activity extends AppCompatActivity {
         profilePic = findViewById(R.id.profilePic);
         profileName = findViewById(R.id.profileName);
         profileNameEdit = findViewById(R.id.profileNameEdit);
+        save = findViewById(R.id.save);
+        discard = findViewById(R.id.cancel);
         profileName.setOnClickListener(v -> {
             profileNameEdit.setVisibility(View.VISIBLE);
+            save.setVisibility(View.VISIBLE);
+            discard.setVisibility(View.VISIBLE);
             profileNameEdit.setText(profileName.getText().toString());
         });
     }
 
     private void OnClick() {
         uploadPic.setOnClickListener(v -> {
+            callPopupWindow(uploadPic);
+        });
+
+        save.setOnClickListener(v -> {
+            String uname = profileNameEdit.getText().toString();
+        });
+        discard.setOnClickListener(v -> {
+            profileNameEdit.setVisibility(View.GONE);
+            save.setVisibility(View.GONE);
+            discard.setVisibility(View.GONE);
+        });
+    }
+    private void callPopupWindow(ImageButton uploadPic){
+        PopupWindow popupWindow = new PopupWindow(this);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        assert inflater != null;
+        View view = inflater.inflate(R.layout.popup_action_profileoptions, null);
+        popupWindow.setContentView(view);
+        popupWindow.setFocusable(true);
+
+        ImageButton Camera = view.findViewById(R.id.camera);
+        ImageButton Files = view.findViewById(R.id.gallery);
+        Camera.setOnClickListener(v -> {
+//                todo
+        });
+        Files.setOnClickListener(v -> {
             SelectImage();
         });
+
+        popupWindow.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
+        popupWindow.setHeight(WindowManager.LayoutParams.MATCH_PARENT);
+        popupWindow.setBackgroundDrawable(null);
+        popupWindow.setElevation(6);
+        popupWindow.setOverlapAnchor(true);
+        popupWindow.showAsDropDown(uploadPic);
     }
 
     private void SelectImage() {
