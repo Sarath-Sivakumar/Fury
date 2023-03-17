c198bee9cf538226c582e07463843ae3185c5e74
Rohith Roy <rohith.roy393@gmail.com>
2023-03-17 11:15:35 +0530

    commit 231

--- app/src/main/java/app/personal/fury/UI/User_Init/walkthroughFrag/frag4.java
+++ app/src/main/java/app/personal/fury/UI/User_Init/walkthroughFrag/frag4.java
@@ -25,6 +25,6 @@ public class frag4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_walkthrough_exp, container, false);
+        return inflater.inflate(R.layout.item_walkthrough_due, container, false);
     }
 }
