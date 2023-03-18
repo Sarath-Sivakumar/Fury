b0aea45b98e775c0214ae4d42b8b61a08fec5d59
Rohith Roy <rohith.roy393@gmail.com>
2023-03-18 15:02:09 +0530

    commit 234

--- app/src/main/java/app/personal/fury/UI/User_Init/walkthroughFrag/frag1.java
+++ app/src/main/java/app/personal/fury/UI/User_Init/walkthroughFrag/frag1.java
@@ -1,5 +1,6 @@
 package app.personal.fury.UI.User_Init.walkthroughFrag;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import app.personal.fury.R;
+import app.personal.fury.UI.User_Init.Landing;
+import app.personal.fury.UI.splashTutorialSlider;
 
 public class frag1 extends Fragment {
+
+    private ImageButton next;
+    private Button skip;
 
     public frag1() {
         // Required empty public constructor
@@ -27,9 +35,19 @@ public class frag1 extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.item_walkthrough_exp, container, false);
         findView(v);
+        onClick();
         return v;
     }
     private void findView(View v){
+        next = v.findViewById(R.id.next);
+        skip = v.findViewById(R.id.skip);
+    }
 
+    private void onClick(){
+        next.setOnClickListener(v -> splashTutorialSlider.setPage(1));
+        skip.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), Landing.class));
+            requireActivity().finish();
+        });
     }
 }
