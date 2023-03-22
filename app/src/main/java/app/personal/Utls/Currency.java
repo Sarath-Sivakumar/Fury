94b021d80a232a06c56d506bfb1542da30dbe3a5
Rohith Roy <rohith.roy393@gmail.com>
2023-03-22 21:58:04 +0530

    commit 271

---
+++ app/src/main/java/app/personal/Utls/Currency.java
@@ -0,0 +1,17 @@
+package app.personal.Utls;
+
+public class Currency {
+
+    private static String Currency;
+
+    public Currency() {
+    }
+
+    public void setCurrency(String currency) {
+        Currency = currency;
+    }
+
+    public static String getCur(){
+        return Currency;
+    }
+}
