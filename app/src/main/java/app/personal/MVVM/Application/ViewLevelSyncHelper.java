f0b9cfdd750f919c79b69eee977bcec3282ba266
Rohith Roy <rohith.roy393@gmail.com>
2023-04-16 21:09:06 +0530

    commit 335

--- app/src/main/java/app/personal/MVVM/Application/ViewLevelSyncHelper.java
+++ app/src/main/java/app/personal/MVVM/Application/ViewLevelSyncHelper.java
@@ -6,6 +6,7 @@ import static app.personal.Utls.Constants.default_int_entity;
 import android.util.Log;
 
 import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,8 +38,12 @@ public class ViewLevelSyncHelper {
     private inHandBalEntity localInHandBal;
     private LaunchChecker localLaunchChecker;
 
+    private final MutableLiveData<Integer> initTask;
+
     public ViewLevelSyncHelper(mainViewModel Mvm, DataSyncViewModel Dvm,
                                AppUtilViewModel AppVm, LifecycleOwner lifecycleOwner) {
+        this.initTask = new MutableLiveData<>();
+        this.initTask.postValue(0);
         this.Mvm = Mvm;
         this.Dvm = Dvm;
         this.AppVm = AppVm;
@@ -58,25 +63,32 @@ public class ViewLevelSyncHelper {
     }
 
     private void init() {
+        Log.e("DataSync-Level(VC)", "Helper init.");
         Mvm.getBalance().observe(lifecycleOwner, balance -> {
             try {
                 localBalance = balance;
+                initTask.postValue(1);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local bank balance");
+                initTask.postValue(1);
             }
         });
         Mvm.getInHandBalance().observe(lifecycleOwner, inHandBal -> {
             try {
                 localInHandBal = inHandBal;
+                initTask.postValue(2);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local in hand balance");
+                initTask.postValue(2);
             }
         });
         Mvm.getBudget().observe(lifecycleOwner, budget -> {
             try {
                 localBudget = budget;
+                initTask.postValue(3);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local budget");
+                initTask.postValue(3);
             }
         });
         Mvm.getExp().observe(lifecycleOwner, expEntityList -> {
@@ -86,8 +98,10 @@ public class ViewLevelSyncHelper {
                 } else {
                     Log.e("DataSync-Level(VC)", "No local expenses");
                 }
+                initTask.postValue(4);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local expenses");
+                initTask.postValue(4);
             }
         });
         Mvm.getSalary().observe(lifecycleOwner, salaryEntityList -> {
@@ -97,8 +111,10 @@ public class ViewLevelSyncHelper {
                 } else {
                     Log.e("DataSync-Level(VC)", "No local salary");
                 }
+                initTask.postValue(5);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local salary");
+                initTask.postValue(5);
             }
         });
         Mvm.getDebt().observe(lifecycleOwner, debtEntityList -> {
@@ -108,15 +124,19 @@ public class ViewLevelSyncHelper {
                 } else {
                     Log.e("DataSync-Level(VC)", "No local salary");
                 }
+                initTask.postValue(6);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "No local debt");
+                initTask.postValue(6);
             }
         });
         AppVm.getCheckerData().observe(lifecycleOwner, launchChecker -> {
             try {
                 localLaunchChecker = launchChecker;
+                initTask.postValue(7);
             } catch (Exception ignored) {
                 Log.e("DataSync-Level(VC)", "Launch load error");
+                initTask.postValue(7);
             }
         });
     }
